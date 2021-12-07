1582fb7249b226a542cc346c17dae41ab2ee5376
HoLLy <dario.db@hotmail.com>
2021-12-07 20:37:26 +0100

    Fix performance for day 7

--- day-7/src/main.rs
+++ day-7/src/main.rs
@@ -13,38 +13,37 @@ impl AdventOfCode for Day7 {
     }
 
     fn solve_1(input: &Self::Input) -> Self::Output {
-        // could join into 1 loop I suppose
-        let min = *input.iter().min().unwrap();
-        let max = *input.iter().max().unwrap();
+        // TODO: may need 2, similar to part 2?
+        let median = {
+            // TODO: there has to be a faster way to get the median!
+            let mut sorted = input.clone();
+            sorted.sort();
+            sorted[sorted.len() / 2]
+        };
 
-        (min..=max).fold(usize::MAX, |acc, i| {
-            acc.min(
-                input
-                    .iter()
-                    .map(|&x| ((x as isize - i as isize).abs() as usize))
-                    .sum(),
-            )
-        })
+        input
+            .iter()
+            .map(|&x| ((x as isize - median as isize).abs() as usize))
+            .sum()
     }
 
     fn solve_2(input: &Self::Input) -> Self::Output {
-        // could join into 1 loop I suppose
-        let min = *input.iter().min().unwrap();
-        let max = *input.iter().max().unwrap();
+        // take the floor and ceil of the average, and try both
+        let avg1 = input.iter().sum::<usize>() / input.len();
+        let avg2 = avg1 + 1;
 
-        (min..=max).fold(usize::MAX, |acc, i| {
-            acc.min(
-                input
-                    .iter()
-                    .map(|&x| calculate_fuel((x as isize - i as isize).abs() as usize))
-                    .sum(),
+        let solutions: (usize, usize) = input.iter().fold((0usize, 0usize), |acc, &x| {
+            (
+                acc.0 + (calculate_fuel((x as isize - avg1 as isize).abs() as usize)),
+                acc.1 + (calculate_fuel((x as isize - avg2 as isize).abs() as usize)),
             )
-        })
+        });
+
+        usize::min(solutions.0, solutions.1)
     }
 }
 
-fn calculate_fuel(input: usize) -> usize {
-    // seems like rust is very smart: https://godbolt.org/z/WscWfvfdr
-    // TODO: figure out this algorithm and implement it myself
-    (1..=input).sum()
+// calculates the triangular number
+const fn calculate_fuel(input: usize) -> usize {
+    input * (input + 1) / 2
 }
