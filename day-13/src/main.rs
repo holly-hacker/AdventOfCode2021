2fc199c9144917fa91669139867f8b3d74a7e590
HoLLy <dario.db@hotmail.com>
2021-12-13 22:27:47 +0100

    Improve speed of part 2

--- day-13/src/main.rs
+++ day-13/src/main.rs
@@ -1,5 +1,6 @@
 use aoc_lib::utils::*;
 use aoc_lib::*;
+use tinyvec::{array_vec, ArrayVec};
 
 // TODO: part 2: FJAHJGAH
 aoc_setup!(Day13, sample 1: 17, sample 2: 16, part 1: 785, part 2: 98);
@@ -10,80 +11,29 @@ pub enum Fold {
     AlongY(usize),
 }
 
+impl Default for Fold {
+    fn default() -> Self {
+        Fold::AlongX(0)
+    }
+}
+
 pub struct FoldableField2D<T> {
     pub field: Field2D<T>,
     pub width: usize,
     pub height: usize,
 }
 
-impl<T> FoldableField2D<T>
-where
-    T: Copy,
-{
-    pub fn new(width: usize, height: usize, value: T) -> Self {
-        let field = Field2D::new(width, height, value);
-        Self {
-            field,
-            width,
-            height,
-        }
-    }
-}
-
-impl FoldableField2D<bool> {
-    pub fn fold_horizontal(&mut self, fold_idx: usize) {
-        ((fold_idx + 1)..(self.height.min(fold_idx * 2 + 1))).for_each(|y| {
-            let target = fold_idx * 2 - y;
-            (0..self.width).for_each(|x| {
-                if self.field[(x, y)] {
-                    self.field[(x, target)] = true;
-                }
-            });
-        });
-        self.height = fold_idx;
-    }
-
-    pub fn fold_vertical(&mut self, fold_idx: usize) {
-        ((fold_idx + 1)..(self.width.min(fold_idx * 2 + 1))).for_each(|x| {
-            let target = fold_idx * 2 - x;
-            (0..self.height).for_each(|y| {
-                if self.field[(x, y)] {
-                    // this seems to panic
-                    self.field[(target, y)] = true;
-                }
-            });
-        });
-        self.width = fold_idx;
-    }
-
-    pub fn count(&self) -> usize {
-        (0..self.height)
-            .flat_map(|y| (0..self.width).map(move |x| (x, y)))
-            .filter(|&(x, y)| self.field.get(x, y))
-            .count()
-    }
-
-    pub fn print(&self) {
-        for y in 0..self.height {
-            for x in 0..self.width {
-                print!("{}", if self.field.get(x, y) { '#' } else { '.' });
-            }
-            println!();
-        }
-    }
-}
-
 pub struct Day13;
 
 impl AdventOfCode for Day13 {
-    type Input = (Vec<(usize, usize)>, Vec<Fold>);
+    type Input = (Vec<(usize, usize)>, ArrayVec<[Fold; 12]>);
     type Output = usize;
 
     fn parse_input(s: &str) -> Self::Input {
         let mut points = vec![];
-        let mut folds = vec![];
+        let mut folds = array_vec![];
         s.lines().fold(false, |acc, line| {
-            if line.len() == 0 {
+            if line.is_empty() {
                 true
             } else if !acc {
                 // part 1
@@ -106,8 +56,6 @@ impl AdventOfCode for Day13 {
     }
 
     fn solve_1(input: &Self::Input) -> Self::Output {
-        // TODO: for day 1, I don't need to do any mutations. I could make a better algorithm!
-
         let fold = input.1[0];
         let mut hashmap = rustc_hash::FxHashSet::default();
         hashmap.reserve(input.0.len());
@@ -128,20 +76,35 @@ impl AdventOfCode for Day13 {
             .0
             .iter()
             .fold((0, 0), |(w, h), &(x, y)| (w.max(x + 1), h.max(y + 1)));
-        let mut field = FoldableField2D::new(width, height, false);
+
+        let (width, height) = input
+            .1
+            .iter()
+            .fold((width, height), |(w, h), &fold| match fold {
+                Fold::AlongX(fold_idx) => (fold_idx, h),
+                Fold::AlongY(fold_idx) => (w, fold_idx),
+            });
+
+        let mut field = Field2D::new(width, height, false);
 
         input
             .0
             .iter()
-            .for_each(|&(x, y)| field.field[(x, y)] = true);
+            .map(|&(x, y)| {
+                input.1.iter().fold((x, y), |(x, y), &fold| match fold {
+                    Fold::AlongX(fold_idx) => (if x > fold_idx { fold_idx * 2 - x } else { x }, y),
+                    Fold::AlongY(fold_idx) => (x, if y > fold_idx { fold_idx * 2 - y } else { y }),
+                })
+            })
+            .for_each(|(x, y)| field[(x, y)] = true);
 
-        input.1.iter().for_each(|fold| match fold {
-            Fold::AlongX(x) => field.fold_vertical(*x),
-            Fold::AlongY(y) => field.fold_horizontal(*y),
-        });
+        for y in 0..height {
+            for x in 0..width {
+                print!("{}", if field.get(x, y) { '#' } else { '.' });
+            }
+            println!();
+        }
 
-        // field.print();
-
-        field.count()
+        field.data.iter().filter(|&&aa| aa).count()
     }
 }
