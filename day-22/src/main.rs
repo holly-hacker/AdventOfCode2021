ab82fd6df59ea688d371224f9153a17c88de2eb8
HoLLy <dario.db@hotmail.com>
2021-12-22 16:53:23 +0100

    Improve perf for day 22

--- day-22/src/main.rs
+++ day-22/src/main.rs
@@ -1,7 +1,7 @@
 use aoc_lib::*;
 use tinyvec::{array_vec, ArrayVec, TinyVec};
 
-aoc_setup!(Day22, sample 1: 474140, sample 2: 2758514936282235, part 1: 568000);
+aoc_setup!(Day22, sample 1: 474140, sample 2: 2758514936282235, part 1: 568000, part 2: 1177411289280259);
 
 #[derive(Debug, Clone, Copy)]
 pub struct Instruction {
@@ -51,11 +51,11 @@ impl Region3D {
     }
 
     // can return up to 3^3 values, if split in the center
-    // for performance reasons, only store up to 4 items on the stack (common case)
-    // this can be increased to 27 if needed
+    // for performance reasons, only store up to 9 items on the stack (handles common cases)
+    // this can be increased to 27 but that seems worse for performance
     pub fn split_excluding_self(self, other: &Self) -> TinyVec<[Self; 9]> {
         self.range_x
-            .split(&other.range_x) // split x range
+            .split(other.range_x) // split x range
             .into_iter()
             .map(|new_region_x| Self {
                 range_x: new_region_x,
@@ -63,7 +63,7 @@ impl Region3D {
             }) // turn split x ranges into
             .flat_map(|x_chunk| {
                 self.range_y
-                    .split(&other.range_y)
+                    .split(other.range_y)
                     .into_iter()
                     .map(move |new_region_y| Self {
                         range_y: new_region_y,
@@ -71,7 +71,7 @@ impl Region3D {
                     })
                     .flat_map(|y_chunk| {
                         self.range_z
-                            .split(&other.range_z)
+                            .split(other.range_z)
                             .into_iter()
                             .map(move |new_region_z| Self {
                                 range_z: new_region_z,
@@ -84,16 +84,15 @@ impl Region3D {
     }
 
     pub fn fits_inside(&self, other: &Self) -> bool {
-        let ret = self.range_x.fits_inside(&other.range_x)
-            && self.range_y.fits_inside(&other.range_y)
-            && self.range_z.fits_inside(&other.range_z);
-        ret
+        self.range_x.fits_inside(other.range_x)
+            && self.range_y.fits_inside(other.range_y)
+            && self.range_z.fits_inside(other.range_z)
     }
 
     pub fn intersects(&self, other: &Self) -> bool {
-        self.range_x.intersects(&other.range_x)
-            && self.range_y.intersects(&other.range_y)
-            && self.range_z.intersects(&other.range_z)
+        self.range_x.intersects(other.range_x)
+            && self.range_y.intersects(other.range_y)
+            && self.range_z.intersects(other.range_z)
     }
 
     pub fn size(&self) -> usize {
@@ -102,12 +101,12 @@ impl Region3D {
 }
 
 #[derive(Default, Clone, Copy, PartialEq, Eq)]
-pub struct Region1D(isize, isize);
+pub struct Region1D(i32, i32);
 
 impl Region1D {
     pub fn parse(text: &str) -> Self {
         let (a, b) = text.split_once("..").unwrap();
-        let pair: (isize, isize) = (a.parse().unwrap(), b.parse().unwrap());
+        let pair: (i32, i32) = (a.parse().unwrap(), b.parse().unwrap());
         Self(pair.0.min(pair.1), pair.0.max(pair.1))
     }
 
@@ -121,38 +120,42 @@ impl Region1D {
     }
 
     /// Splits the Region1D into pieces. One of the regions will perfectly cover the other region.
-    // TODO: rewrite to be cleaner using inclusiverange::contains?
-    pub fn split(self, other: &Self) -> ArrayVec<[Self; 3]> {
-        if other.1 < self.0 || other.0 > self.1 {
-            // other is definitely outside
-            array_vec![[Self; 3] => self]
-        } else if other.0 <= self.0 && other.1 >= self.1 {
+    pub fn split(self, other: Self) -> ArrayVec<[Self; 3]> {
+        if other.0 <= self.0 && other.1 >= self.1 {
             // other completely covers us (ie. we are contained in them)
-            array_vec![[Self; 3] => self]
-        } else if other.0 >= self.0 && other.1 <= self.1 {
-            // other is definitely inside
-            array_vec![[Self; 3] => Self(self.0, other.0 - 1), *other, Self(other.1 + 1, self.1)]
-        } else {
-            // other partially overlaps, covering one of our edges
-            if other.0 < self.0 {
+            return array_vec![[Self; 3] => self];
+        }
+
+        match (self.contains(other.0), self.contains(other.1)) {
+            (false, false) => {
+                // no overlap
+                array_vec![[Self; 3] => self]
+            }
+            (true, true) => {
+                // other is definitely inside
+                array_vec![[Self; 3] => Self(self.0, other.0 - 1), other, Self(other.1 + 1, self.1)]
+            }
+            (false, true) => {
                 // other covers left edge
                 array_vec![[Self; 3] => Self(self.0, other.1), Self(other.1 + 1, self.1)]
-            } else {
+            }
+            (true, false) => {
                 // other covers right edge
                 array_vec![[Self; 3] => Self(self.0, other.0 - 1), Self(other.0, self.1)]
             }
         }
     }
 
-    pub fn fits_inside(&self, other: &Self) -> bool {
+    pub fn fits_inside(&self, other: Self) -> bool {
         self.0 >= other.0 && self.1 <= other.1
     }
 
-    pub fn intersects(&self, other: &Self) -> bool {
-        (self.0..=self.1).contains(&other.0)
-            || (self.0..=self.1).contains(&other.1)
-            || (other.0..=other.1).contains(&self.0)
-            || (other.0..=other.1).contains(&self.1)
+    pub fn intersects(&self, other: Self) -> bool {
+        self.0.max(other.0) <= self.1.min(other.1)
+    }
+
+    fn contains(&self, other: i32) -> bool {
+        (self.0..=self.1).contains(&other)
     }
 
     pub fn size(&self) -> usize {
@@ -186,9 +189,13 @@ impl RegionCollection {
             if self.regions[i].intersects(&to_fit) {
                 let new_regions = self.regions[i].split_excluding_self(&to_fit);
 
-                // PERF: can get rid of this remove
-                self.regions.remove(i);
-                self.regions.extend_from_slice(new_regions.as_slice());
+                if new_regions.is_empty() {
+                    self.regions.remove(i);
+                } else {
+                    // for performance reasons, insert the first new region in the old region's position to prevent having to shift all items after it
+                    self.regions[i] = new_regions[0];
+                    self.regions.extend_from_slice(&new_regions.as_slice()[1..]);
+                }
             }
         }
 
@@ -253,31 +260,31 @@ fn test_region_size() {
 fn test_split_region1d() {
     assert_eq!(
         [Region1D(5, 10)],
-        Region1D(5, 10).split(&Region1D(20, 30)).as_slice(),
+        Region1D(5, 10).split(Region1D(20, 30)).as_slice(),
         "outside"
     );
 
     assert_eq!(
         [Region1D(5, 10)],
-        Region1D(5, 10).split(&Region1D(5, 10)).as_slice(),
+        Region1D(5, 10).split(Region1D(5, 10)).as_slice(),
         "covers completely"
     );
 
     assert_eq!(
         [Region1D(5, 7), Region1D(8, 10)],
-        Region1D(5, 10).split(&Region1D(0, 7)).as_slice(),
+        Region1D(5, 10).split(Region1D(0, 7)).as_slice(),
         "covers left"
     );
 
     assert_eq!(
         [Region1D(5, 7), Region1D(8, 10)],
-        Region1D(5, 10).split(&Region1D(8, 20)).as_slice(),
+        Region1D(5, 10).split(Region1D(8, 20)).as_slice(),
         "covers right"
     );
 
     assert_eq!(
         [Region1D(5, 6), Region1D(7, 8), Region1D(9, 10)],
-        Region1D(5, 10).split(&Region1D(7, 8)).as_slice(),
+        Region1D(5, 10).split(Region1D(7, 8)).as_slice(),
         "covers center"
     );
 }
